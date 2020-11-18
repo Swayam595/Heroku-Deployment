277454cf77803e569467e9ad2b0708a25fcfaa84
Swayam Swaroop Mishra <swayam@Swayams-MacBook-Pro.local>
2020-11-17 19:38:18 -0800

    Added store endpoint.

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ api.add_resource(ItemList, '/items')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(UserRegister, '/register')
 api.add_resource(UserList, '/users')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
