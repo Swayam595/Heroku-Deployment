accbefc9d7a512dfedabe62694b244abd76456f9
Swayam Swaroop Mishra <swayam@Swayams-MacBook-Pro.local>
2020-11-17 19:13:58 -0800

    Fixed the db import error.

--- app.py
+++ app.py
@@ -5,18 +5,12 @@ from flask_jwt import JWT
 from security import authentication, identity
 from resources.user import UserRegister, UserList
 from resources.item import Item, ItemList
-from db import db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'secret_key'
 api = Api(app)
-
-#### Creates the table if not present
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authentication, identity)   # JWT creates a new end point i.e. /auth
 
@@ -27,5 +21,4 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(UserList, '/users')
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
